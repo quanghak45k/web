618e52b7eb0d039ce217da2a74003d5cd4c35fde
quangha <quanghak45k@gmail.com>
2022-07-24 09:22:41 +0700

    revised docker-compose.yml ''    context: ./routes/8.1

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
@@ -23,28 +13,28 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
-        <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="fas fa-search"></i>
-            </a>
-            <div class="navbar-search-block">
-                <form class="form-inline">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </li>
+{{--        <li class="nav-item">--}}
+{{--            <a class="nav-link" data-widget="navbar-search" href="#" role="button">--}}
+{{--                <i class="fas fa-search"></i>--}}
+{{--            </a>--}}
+{{--            <div class="navbar-search-block">--}}
+{{--                <form class="form-inline">--}}
+{{--                    <div class="input-group input-group-sm">--}}
+{{--                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">--}}
+{{--                        <div class="input-group-append">--}}
+{{--                            <button class="btn btn-navbar" type="submit">--}}
+{{--                                <i class="fas fa-search"></i>--}}
+{{--                            </button>--}}
+{{--                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">--}}
+{{--                                <i class="fas fa-times"></i>--}}
+{{--                            </button>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </form>--}}
+{{--            </div>--}}
+{{--        </li>--}}
 
-        <!-- Messages Dropdown Menu -->
+        <!-- Admin Dropdown Menu -->
         <li class="nav-item dropdown">
             <div class="nav-item dropdown">
                 <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle user-action"><img src="#" class="avatar" alt=""> Admin <b class="caret"></b></a>
